85e68e4e7d97b35d401406f14e84adf681f95943
nguyen-tuan-minh <65720036+nguyen-tuan-minh@users.noreply.github.com>
2021-04-12 22:06:26 +0700

    Change

--- index3.js
+++ index3.js
@@ -1,12 +1,24 @@
 var request = new XMLHttpRequest()
 request.open('GET', 'https://opentdb.com/api.php?amount=5&category=21&difficulty=easy&type=multiple', true)
-quiz={}
 request.onload = function () {
   var data = JSON.parse(this.response)
 
   if (request.status >= 200 && request.status < 400) {
     console.log(data)
-    
+    quiz=data.results
+    questionair=document.getElementById("questionair")
+    for (let i = 0; i < quiz.length; i++) {
+      questionair.innerHTML+=`
+      <div style="font-size:large ; font-weight: 700">${"Question "+(i+1)+":"}</div>
+      <div>${quiz[i].question} </div>
+      <div>
+      <button style="color : green">${quiz[i].correct_answer}</button>
+      <button>${quiz[i]["incorrect_answers"][0]}</button>
+      <button>${quiz[i]["incorrect_answers"][1]}</button>
+      <button>${quiz[i]["incorrect_answers"][2]}</button>
+      </div>
+      `
+    }
   } else {
     console.log('error')
   }
